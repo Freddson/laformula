e53e99e8bd4732b0a6b54cf7abdf5259ec5979af
Freddson <mikipolu@gmail.com>
2025-02-02 00:56:14 +0100

    FEATURE: Proper parse of first part of provided practice session data

--- test.cpp
+++ test.cpp
@@ -1,15 +1,30 @@
 #include <string>
 #include <iostream>
+#include <vector>
 #include "cpr/cpr.h"
 #include "nlohmann/json.hpp"
 using json = nlohmann::json;
 
 int main() {
-    std::string a, b;
+    std::string a, b, t;
     std::getline(std::cin, a);
-    b = "https://api.openf1.org/v1/drivers?driver_number=" + a;
+    b = "https://api.openf1.org/v1/sessions?country_name=" + a + "&session_type=Practice&year=2024";
     cpr::Response r = cpr::Get(cpr::Url{b});
-    json j = json::parse(r.text);
+    //split response to three parts
+    std::string text = r.text;
+    std::vector<std::string> splitresponse;
+    auto pos = text.find("},");
+    while(pos != std::string::npos) {
+        splitresponse.push_back(text.substr(0, pos));
+        text.erase(0, pos + 2);
+        pos = text.find("},");
+    }
+
+    text = splitresponse[0] + "}";
+    text.erase(text.begin());
+    json j = json::parse(text);
     std::cout << j.dump(4) << std::endl;
+    //If the correct country is provided, pull the last session_key and save all three (by deducting 1 from each session_key)
+    
     return 0;
 }
