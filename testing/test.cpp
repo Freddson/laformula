f278314b8f785a4489cb3b4f2393ec7773376e46
Freddson <mikipolu@gmail.com>
2025-02-08 23:17:19 +0100

    (unfinished) FEATURE: Country-specific if statements for specific races

--- testing/test.cpp
+++ testing/test.cpp
@@ -10,7 +10,20 @@ int main() {
     std::string a, b;
     int sk[3];
     std::getline(std::cin, a);
+    //Switch for race name specification; hurts to write but can't use switch statements on strings
+    
+    if(a == "Miami") a = "United%20States&location=Miami";
+
+    else if(a == "Austin") a = "United%20States&location=Austin";
+
+    else if(a == "Las Vegas") a = "United%20States&location=Las%20Vegas";
+
+    else if (a == "Imola") a = "Italy&location=Imola";
+
+    else if(a == "Monza") a = "Italy&location=Monza";
+
     b = "https://api.openf1.org/v1/sessions?country_name=" + a + "&session_type=Practice&year=2024";
+    std::cout << b << std::endl;
     cpr::Response r = cpr::Get(cpr::Url{b});
     //split response to three parts
     std::string text = r.text;
