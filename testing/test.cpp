3fd52b8a0d676dde4780be34709f369f8954da84
Freddson <mikipolu@gmail.com>
2025-02-10 23:11:17 +0100

    FEATURE: Finished proper sprint detection with save of one practice session key

--- testing/test.cpp
+++ testing/test.cpp
@@ -9,16 +9,17 @@ using json = nlohmann::json;
 int main() {
     std::string a, b;
     int sk[3];
+    int sprint_sk;
+    bool sprint = false;
     std::getline(std::cin, a);
     //Switch for race name specification; hurts to write but can't use switch statements on strings
-    
-    if(a == "Miami") a = "United%20States&location=Miami";
+    if(a == "Miami") { a = "United%20States&location=Miami"; sprint = true;}
 
-    else if(a == "Austin") a = "United%20States&location=Austin";
+    else if(a == "Austin") { a = "United%20States&location=Austin"; sprint = true;}
 
-    else if(a == "Las Vegas") a = "United%20States&location=Las%20Vegas";
+    else if(a == "Las Vegas") a = "United%20States&location=Las%20Vegas"; 
 
-    else if (a == "Imola") a = "Italy&location=Imola";
+    else if(a == "Imola") a = "Italy&location=Imola";
 
     else if(a == "Monza") a = "Italy&location=Monza";
 
@@ -28,13 +29,34 @@ int main() {
     //split response to three parts
     std::string text = r.text;
     std::vector<std::string> splitresponse;
-    auto pos = text.find("},");
-    while(pos != std::string::npos) {
-        splitresponse.push_back(text.substr(0, pos));
-        text.erase(0, pos + 2);
-        pos = text.find("},");
+    if(!sprint) {
+        auto pos = text.find("},");
+        while(pos != std::string::npos) {
+            splitresponse.push_back(text.substr(0, pos));
+            text.erase(0, pos + 2);
+            pos = text.find("},");
+        }
     }
-    if(splitresponse.size() > 3) {
+
+    else if(sprint == true) {
+        splitresponse.push_back(text);
+    }
+
+    std::cout << "SIZE: " << splitresponse.size() << std::endl;
+    //sprint detection
+    if(splitresponse.size() < 2) {
+        std::cout << "Selected race weekend is a sprint weekend.\nResults may be less accurate until proper algorithm correction for sprint races.\n";
+        text = splitresponse[0];
+        text = text.substr(0, text.length() - 1);
+        text = text.substr(1, text.length());
+        json j = json::parse(text);
+        for (auto it = j.begin(); it != j.end(); ++it) {
+            if(it.key() == "session_key") sprint_sk = j["session_key"].get<int>();
+        }
+        std::cout << "SPRINT!\n" << sprint_sk << std::endl; 
+        return 0;
+    }
+    else if(splitresponse.size() > 3) {
         std::cout << "Input is country with more than one race in race calendar!\nSpecify the name of the race (ex. 'Imola', 'Miami').\n";
         return 0;
     }
