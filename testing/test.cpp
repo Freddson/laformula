1042e60daaff5d3b36afc8d51afd5c36750761a6
Freddson <mikipolu@gmail.com>
2025-09-30 23:27:31 +0200

    FEAT: Change return value to struct for easier readability and accessibility, disable sprints for the foreseeable future

--- testing/test.cpp
+++ testing/test.cpp
@@ -4,11 +4,14 @@
 
 using namespace std;
 
+
 int main() {
     std::string a, b;
     int sk[3];
     int sprint_sk;
 
     std::getline(std::cin, a);
-    int skibidi = ReturnSessionKey(a);
+    vals result = ReturnSessionKey(a);
+    //^ this works, second step from roadmap to come!
+    sk[0] = result.sk;
 }
